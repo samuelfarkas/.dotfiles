c9a4c9fd4ce225177e69fce5629023854e83d905
Samuel Farkas <samuel@advixory.com>
2025-07-07 22:23:32 +0200

    aerospace

--- nvim/.config/nvim/after/plugin/null-ls.lua
+++ nvim/.config/nvim/after/plugin/null-ls.lua
@@ -4,6 +4,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
         require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.formatting.eslint_d"),
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.phpcsfixer,
         null_ls.builtins.completion.spell,
